c1fb20c42f031e96ee9b2e13427eefde83486e2c
Nipun Paradkar <nipunparadkar123@gmail.com>
2021-07-10 13:40:55 +0530

    Squish all method setup tests into one

--- src/__tests__/remote-request.ts
+++ src/__tests__/remote-request.ts
@@ -27,73 +27,22 @@ describe("constructor()", function() {
     new RemoteRequest("/get").send();
   });
 
-  it("can setup a get|GET request", function() {
+  it.each([
+    ["get", "GET"], ["post", "POST"],
+    ["put", "PUT"], ["patch", "PATCH"],
+    ["delete", "DELETE"]
+  ])("can setup a %s|%s request", function(lowercased, uppercased) {
     expect.assertions(2);
 
-    mock.get("/get", function(req, res) {
-      expect(req.method()).toEqual("GET");
+    const path = "/" + lowercased;
+    mock.use(lowercased, path, function(req, res) {
+      expect(req.method()).toEqual(uppercased);
 
       return res.status(200);
     });
 
-    ["get", "GET"].forEach(function(method) {
-      new RemoteRequest("/get", method).send();
-    });
-  });
-
-  it("can setup a post|POST request", function() {
-    expect.assertions(2);
-
-    mock.post("/post", function(req, res) {
-      expect(req.method()).toEqual("POST");
-
-      return res.status(200);
-    });
-
-    ["post", "POST"].forEach(function(method) {
-      new RemoteRequest("/post", method).send();
-    });
-  });
-
-  it("can setup a put|PUT request", function() {
-    expect.assertions(2);
-
-    mock.put("/put", function(req, res) {
-      expect(req.method()).toEqual("PUT");
-
-      return res.status(200);
-    });
-
-    ["put", "PUT"].forEach(function(method) {
-      new RemoteRequest("/put", method).send();
-    });
-  });
-
-  it("can setup a patch|PATCH request", function() {
-    expect.assertions(2);
-
-    mock.patch("/patch", function(req, res) {
-      expect(req.method()).toEqual("PATCH");
-
-      return res.status(200);
-    });
-
-    ["patch", "PATCH"].forEach(function(method) {
-      new RemoteRequest("/patch", method).send();
-    });
-  });
-
-  it("can setup a delete|DELETE request", function() {
-    expect.assertions(2);
-
-    mock.delete("/delete", function(req, res) {
-      expect(req.method()).toEqual("DELETE");
-
-      return res.status(200);
-    });
-
-    ["delete", "DELETE"].forEach(function(method) {
-      new RemoteRequest("/delete", method).send();
+    [lowercased, uppercased].forEach(function(method) {
+      new RemoteRequest(path, method).send();
     });
   });
 
